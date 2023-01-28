49ada3d654831618ad02117afe68743345fc0bd2
bvb1500 <123811276+bvb1500@users.noreply.github.com>
2023-01-28 13:26:28 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,50 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  
+  <title></title>
+  <link rel="stylesheet" href="index/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+</head>
+<body>
+<!-- partial:index.partial.html --><center><br>
+<div class="product">
+  <img class="product" src="index/PROD.png" width="350px">
+</div>
+<h1>Alpilean™</h1>
+<div class="hr">
+  <hr width="60%">
+</div>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+<div class="wrapper">
+  <div class="select-btn">
+    <span>Select Country</span>
+    <i class="uil uil-angle-down"></i>
+  </div>
+  <div class="content">
+    <div class="search">
+      <i class="uil uil-search"></i>
+      <input spellcheck="false" type="text" placeholder="Search">
     </div>
-  )
-}
+    <ul class="options"><li id="Australia" class="">Australia</li><li id="Canada" class="">Canada</li><li id="United States" class="">United States</li></ul>
+  </div>
+  <div class="geo">
+    <img src="index/geos.png" width="100%">
+  </div>
+</div><br>
+  <footer class="py-3 text-center">
+<div class="container"><br>
+  <div class="trust">
+    <img src="index/trust.png" width="100%">
+  </div>
+  <hr width="60%">
+  <p class="text-center"><a rel="nofollow" href="index/privacy.html" target="_blank">Privacy Policy</a> - <a href="index/terms.html">Terms &amp; Conditions</a>
+    </p><p class="fs-6">Copyright 2023 All Right Reserved</p>
+</div>
+</footer>
+</center>
+<!-- partial -->
+  <script src="index/script.js"></script>
+
+
+
+</body></html>
